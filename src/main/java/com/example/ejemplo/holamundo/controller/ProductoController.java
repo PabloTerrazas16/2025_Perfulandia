bf1d85eb1b284250b5ef9d66a328ec5ce417d5d7
SSDD <pa.terrazas@duocuc.cl>
2025-04-23 20:13:02 -0400

    primer commit

--- src/main/java/com/example/ejemplo/holamundo/controller/ProductoController.java
+++ src/main/java/com/example/ejemplo/holamundo/controller/ProductoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ProductoController {
 
     @GetMapping
-    public String index() {
+    public String index1() {
         return "bienvenido al lista de productos";
     }
 
