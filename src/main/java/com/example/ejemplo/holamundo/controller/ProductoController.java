78987530725ec652077dcf1df4bc98d7ad402a9f
SSDD <pa.terrazas@duocuc.cl>
2025-04-23 20:13:43 -0400

    se cambia de index a index2

--- src/main/java/com/example/ejemplo/holamundo/controller/ProductoController.java
+++ src/main/java/com/example/ejemplo/holamundo/controller/ProductoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ProductoController {
 
     @GetMapping
-    public String index1() {
+    public String index2() {
         return "bienvenido al lista de productos";
     }
 
